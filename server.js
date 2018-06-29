34fb73c245bf4d504a08977a79f92608a74a598f
Shane Orend <wagen00@gmail.com>
2018-06-29 13:30:16 -0400

    skeleton done

---
+++ server.js
@@ -0,0 +1,36 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+// var mongojs = require("mongojs"); 
+// var databaseUrl = "photodb";
+// var collections = ["inventory"];
+// var db = mongojs(databaseUrl, collections);
+var mongoose = require("mongoose");
+
+var db = require("./models/photo.js");
+const PORT = process.env.PORT || 3000;
+app.use(express.static(__dirname + "/public"));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Enable CORS so that browsers don't block requests.
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    next();
+});
+mongoose.Promise = Promise;
+mongoose.connect("mongodb://localhost/photodb", {
+  useMongoClient: true
+});
+//subject to change
+require("./routes/htmlRoutes")(app);
+// require("./routes/apiRoutes")(app);
+
+
+
+app.listen(PORT, function() {
+    console.log("server listening on Port " + PORT);
+});
